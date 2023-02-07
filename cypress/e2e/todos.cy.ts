08d8e0e3759c17783ad79564abdbbb82176276cf
Jenny Svensson <114919739+Jenny-Svensson@users.noreply.github.com>
2023-02-07 14:15:06 +0100

    :white_check_mark: test if input is empty

--- cypress/e2e/todos.cy.ts
+++ cypress/e2e/todos.cy.ts
@@ -17,13 +17,37 @@ describe("should test to see if all elements are there", () => {
 });
 
 describe("should add movies into movie-container", () => {
-  it("should add a movies", () => {
-
+  it("should be able to type", () => {
     cy.get("input").type("Star Wars").should("have.value", "Star Wars"); // söker efter filmer som heter Star Wars
-    cy.get("#searchForm").submit(); // Submitta en form
-    cy.get("h3").contains("Star Wars"); // söker om alla h3 taggar innehåller Star Wars
 
     });
+
+  it("should search for movie ", () => {
+    cy.get("input").type("Star Wars").should("have.value", "Star Wars");
+    cy.get("#searchForm").submit(); // Submitta en form
+    cy.get("h3").contains("Star Wars"); // söker om alla h3 taggar innehåller Star Wars
+    cy.get("h3").should("exist"); // söker så att h3 tag finns
+    cy.get("img").should("exist"); // söker så att en img tag finns
+
+  });
+
+describe("should create p-tag with follow message: Inga sökresultat att visa ", () => {
+  it("should create p-tag if input empty ", () => {
+    cy.get("input").should("have.value", ""); // input har inget värde
+  });
+
+  it("should create a p-tag and show inga sökresultat att visa", () => {
+    cy.get("input").should("have.value", ""); 
+    cy.get("#searchForm").submit(); // trycker på knappen
+    cy.get("p").contains("Inga sökresultat att visa"); // då dyker en p-tag upp med följande meddelande
+  });
+
+
+  });
+
+
+
+
   
 
 
